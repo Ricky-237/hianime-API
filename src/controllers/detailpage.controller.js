7aa90de9b6d1e1b857fc7595d3abbf0a26cf506b
Ricky-237 <110074174+Ricky-237@users.noreply.github.com>
2025-08-20 21:57:08 +0100

    Update fusion detailpage.controller.js and episodes.controller

--- src/controllers/detailpage.controller.js
+++ src/controllers/detailpage.controller.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import config from '../config/config';
+import { extractEpisodes } from '../extractor/extractEpisodes';
 import { extractDetailpage } from '../extractor/extractDetailpage';
 import { axiosInstance } from '../services/axiosInstance';
 import { validationError } from '../utils/errors';
@@ -8,8 +11,36 @@ const detailpageController = async (c) => {
   if (!result.success) {
     throw new validationError(result.message, 'maybe id is incorrect : ' + id);
   }
-  const response = extractDetailpage(result.data);
+  
+const episodesController = async () => {
+  if (!id) throw new validationError('id is required');
 
+  const Referer = `/watch/${id}`;
+  const idNum = id.split('-').at(-1);
+  const ajaxUrl = `/ajax/v2/episode/list/${idNum}`;
+
+  try {
+    const { data } = await axios.get(config.baseurl + ajaxUrl, {
+      headers: {
+        Referer: Referer,
+        ...config.headers,
+      },
+    });
+
+    const response = extractEpisodes(data.html);
+    return response;
+  } catch (err) {
+    console.log(err.message);
+
+    throw new validationError('make sure the id is correct', { validIdEX: 'one-piece-100' });
+  }
+};
+
+  const _response = extractDetailpage(result.data);
+  const response = { 
+    info: _response,
+    episodes: episodesController
+  };
   return response;
 };
 
