9d0cab17dba8d4f07025d2bccac8eb125416cc1b
Ricky-237 <110074174+Ricky-237@users.noreply.github.com>
2025-08-20 22:09:52 +0100

    Update detailpage.controller.js

--- src/controllers/detailpage.controller.js
+++ src/controllers/detailpage.controller.js
@@ -12,34 +12,23 @@ const detailpageController = async (c) => {
     throw new validationError(result.message, 'maybe id is incorrect : ' + id);
   }
   
-const episodesController = async () => {
-  if (!id) throw new validationError('id is required');
-
-  const Referer = `/watch/${id}`;
+const Referer = `/watch/${id}`;
   const idNum = id.split('-').at(-1);
   const ajaxUrl = `/ajax/v2/episode/list/${idNum}`;
 
-  try {
-    const { data } = await axios.get(config.baseurl + ajaxUrl, {
-      headers: {
-        Referer: Referer,
-        ...config.headers,
-      },
-    });
+  const { data } = await axios.get(config.baseurl + ajaxUrl, {
+    headers: {
+      Referer: Referer,
+      ...config.headers,
+    },
+  });
 
-    const response = extractEpisodes(data.html);
-    return response;
-  } catch (err) {
-    console.log(err.message);
-
-    throw new validationError('make sure the id is correct', { validIdEX: 'one-piece-100' });
-  }
-};
-
+  const episodes = extractEpisodes(data.html);
+  
   const _response = extractDetailpage(result.data);
   const response = { 
     info: _response,
-    episodes: episodesController
+    episodes: episodes
   };
   return response;
 };
